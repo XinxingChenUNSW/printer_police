4a3abf4038c95672f34cfe99b06acb576d55b05c
Daniel Lin <z5209697@ad.unsw.edu.au>
2022-06-29 11:57:13 +1000

    Pushing updated wifi

--- data_py/data_rcv.py
+++ data_py/data_rcv.py
@@ -12,8 +12,15 @@ s.listen(0)
 print(s.getsockname())
 
 # Define start and stop bytes
-start_bytes = "COVID".encode('utf-8')
+start_bytes = "START".encode('utf-8')
 stop_bytes = "DOVIC".encode('utf-8')
+
+num_bytes = [2, 12, 1]
+data_type = ['f', 'f', 'i']
+
+# load_cell_bytes = 2
+# imu_bytes = 12
+# encoder_bytes = 1
 
 total_size = 5
 
@@ -26,7 +33,7 @@ while True:
     print("Connected to client, writing to csv...")
 
     while True:
-        content = client.recv(60)
+        content = client.recv()
 
         if len(content) == 0:
             break
@@ -45,13 +52,19 @@ while True:
 
         data_row = []
 
-        # Decode bytes
-        for _ in range(total_size):
-            val = unpack('f', content[curr_ptr: curr_ptr+4])[0]
+        for size, idx in enumerate(num_bytes):
+            byteSize = size * 4
+            val = unpack(data_type[idx], content[curr_ptr: curr_ptr + byteSize])
             data_row.append(val)
-            curr_ptr += 4
-            
-        writer.writerow(data_row)
+            curr_ptr += byteSize
+
+        # Decode bytes
+        # for _ in range(total_size):
+        #     val = unpack('f', content[curr_ptr: curr_ptr+4])[0]
+        #     data_row.append(val)
+        #     curr_ptr += 4
+        print(data_row)
+        # writer.writerow(data_row)
 
     print("Closing connection")
     client.close()
