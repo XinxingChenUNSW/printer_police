b74a2048e2bd1a5d655a9171e50324b5ca390bd5
Naedos <37355708+Naedos@users.noreply.github.com>
2022-07-10 19:27:23 +1000

    some parsing changes

--- smoothdata.py
+++ smoothdata.py
@@ -6,7 +6,7 @@ import csv
 
 # formats the csv data as numpy arrays of type np.float64
 def format_np(list_):
-    list_ = np.array(list_)                         # convert raw laser data to numpy array
+    list_ = np.array(list_,dtype=object)                         # convert raw laser data to numpy array
     list_ = list_.astype(np.float64)                # cast to numpy.float64
     return list_
 
@@ -14,7 +14,7 @@ def format_np(list_):
 f_test = open('test1.csv', 'r')
 f_laser = open('Laser_test1.csv', 'r')
 # read as csv
-csv_test = csv.reader(f_test, delimiter=" ")
+csv_test = csv.reader(f_test, delimiter=' ')
 csv_laser = csv.reader(f_laser)
 # convert and store in list
 list_test = list(csv_test)
@@ -25,14 +25,19 @@ np.set_printoptions(suppress=True)
 
 # cleaning the data
 # test
-list_test = format_np(list_test)
-
-# laser
-laser_headings = list_laser[24]                     # in case we need the headings of the laser data
-list_laser = format_np(list_laser[25:])             # grab the raw data from the laser data files and clean it
-
+for row in list_test:
+    row.pop()
 
 print(list_test[0])
+list_test = np.array(list_test[0:-1])
+print(list_test)
+# temp = np.array(list_test[0])
+# temp = temp.astype(np.float64)
+# laser
+laser_headings = list_laser[24]                     # in case we need the headings of the laser data
+print(list_laser[26])
+list_laser = format_np(list_laser[25:])             # grab the raw data from the laser data files and clean it
+
 
 # plotting
 plt.figure()
