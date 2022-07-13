28e7c2f384d5ecb6c7055386df31832ff0362eb0
Naedos <37355708+Naedos@users.noreply.github.com>
2022-07-13 23:36:16 +1000

    applied basic thresholding within 50% of current mean and an average damper of 1

--- Python/rollingAverage.py
+++ Python/rollingAverage.py
@@ -30,16 +30,26 @@ def rolling_average(rolling_a_q: Queue, csv_queue: Queue, plot_queue: Queue):
     dataGeneratorMatrix = []
     dataRowGenerator = 1
     windowSize = 11
+    threshold_percent = 0.5 # a percentage of the currently tracked mean that the new data must be within to affect the rolling average
+    outlier_damp = 1
     # The number of moving averages you have found
     index = 0
-
+    # threshold flag
+    first_run = True
+    
     while True:
 
         # with open("test.csv", "r") as readfile:
         #     reader = csv.reader(readfile)
         #     for row in reader:
         while not rolling_a_q.empty():
+            # grab a row of data 
             row = rolling_a_q.get()
+            if (first_run == True):
+                threshold_flags = [False] * (len(row)-1)
+                prev_mean = [00.00] * (len(row)-1)
+                data_out = [00.00] * (len(row)-1)
+                first_run = False
             # time.sleep(0.01)
             # # remove white spaces from test data and convert string to float
             # row = list(map(float, row[0].split()))
@@ -48,21 +58,59 @@ def rolling_average(rolling_a_q: Queue, csv_queue: Queue, plot_queue: Queue):
             #     writer = csv.writer(dataRawfile)
             #     writer.writerow(row)
 
-            # append row to dataGeneratorMatrix
+            # append row to dataGeneratorMatrix to keep track of data
             dataGeneratorMatrix.append(row)
-
+            # increment the index that keeps track of dataGeneratorMatrix
             dataRowGenerator = dataRowGenerator + 1
 
             # for every valid data with required window size compute moving average
-            if (index < len(dataGeneratorMatrix) - windowSize + 1):
+            if (index < len(dataGeneratorMatrix) - windowSize + 1): 
 
                 # Store elements from i to i+window_size
-                # in list to get the current window
+                # in list to get the current window for every data point
+                # grabs the last windowSize (11 for example) samples 
                 window = np.array(dataGeneratorMatrix[index : index  + windowSize])
-                # Calculate the average of current window (not avaeraging the timeStep)
-                windowAverage = np.mean(window[:,1:len(row)], axis = 0)
+                for data_idx in range(threshold_flags):
+                    if (threshold_flags[data_idx] == False):
+                        # dont threshold
+                        # compute average
+                        data_out[data_idx] = np.mean(window[:, data_idx+1], axis = 0)
+                        # store this average
+                        prev_mean[data_idx] = data_out[data_idx]
+                        # allow threshold flag, change if targeting certain data for thresholding
+                        threshold_flags[data_idx] = True
+                    else:
+                        # if latest data is OUTSIDE threshold
+                        if (abs(dataGeneratorMatrix[dataRowGenerator - (windowSize - 1)][data_idx+1] - prev_mean[data_idx]) > threshold_percent*prev_mean[data_idx]):
+                            # simply set the data output to the value outside threshold
+                            data_out[data_idx] = dataGeneratorMatrix[dataRowGenerator - (windowSize - 1)][data_idx+1]
+                            # however, we cant just ignore the value since it might be valid, 
+                            # so apply a weighting to reduce the impact on the mean
+                            temp = window[:, data_idx+1]
+                            temp = temp[-1, data_idx+1]*outlier_damp
+                            prev_mean[data_idx] = np.mean(window[:, data_idx+1], axis = 0)
 
-                timeStep = dataGeneratorMatrix[dataRowGenerator - (windowSize - 1)][0]
+                        else:
+                            # compute average
+                            data_out[data_idx] = np.mean(window[:, data_idx+1], axis = 0)
+                            # store this average
+                            prev_mean[data_idx] = data_out[data_idx]
+                            # allow threshold flag, change if targeting certain data for thresholding
+                            threshold_flags[data_idx] = True
+
+                # if (threshold_flag == False):
+                #     # dont threshold because either:
+                #     #                       - theres no currently tracked average because we just started
+                #     #                       - the prev_mean was reset and so no threshold 
+                #     windowAverage = np.mean(window[:,1:len(row)], axis = 0)
+                #     prev_mean = windowAverage
+                # else: # check data if we need to threshold and also check if prev_mean must be reset
+                #     # threshold means if the latest data point is too far off from 
+                #     # the previous mean, output the raw data as valid data
+                #     if (window[-1,1:len(row)] - prev_mean)
+                # # Calculate the average of current window (not avaeraging the timeStep)
+                # windowAverage = np.mean(window[:,1:len(row)], axis = 0)
+
 
                 # Store the average of current
                 # window in moving average list
@@ -75,7 +123,6 @@ def rolling_average(rolling_a_q: Queue, csv_queue: Queue, plot_queue: Queue):
                 # print(loads)
                 # windowAverage = [timeStep] + loads + gyro1 + gyro2 + acc1 + acc2 + windowAverage[14:]
 
-                windowAverage = np.insert(windowAverage,0, timeStep)
 
                 # with open("dataAverage.csv", 'a') as datafile:
                 #     writer = csv.writer(datafile)
@@ -83,8 +130,10 @@ def rolling_average(rolling_a_q: Queue, csv_queue: Queue, plot_queue: Queue):
 
                 
 
-                csv_queue.put(windowAverage)
-                plot_queue.put(windowAverage)
+                timeStep = dataGeneratorMatrix[dataRowGenerator - (windowSize - 1)][0]
+                full_data = np.insert(data_out, 0, timeStep)
+                csv_queue.put(full_data)
+                plot_queue.put(full_data)
 
                 index+=1
 
